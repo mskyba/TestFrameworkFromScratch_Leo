b5ced7062d55228a384f0b43fa3d453c919caec9
Leonid Perevoznyk <leonid.perevoznik@gmail.com>
2022-08-09 10:29:16 +0300

    Add remote run

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-* To run tests execute `mvn clean test`
-* To run server with report `allure serve target/allure-results`
-* To generate report ` allure generate target/allure-results`
+* To run tests execute 
+`mvn clean test`
+* To run server with report 
+`allure serve target/allure-results`
+* To generate report 
+`allure generate target/allure-results`
--- src/test/java/selenideexample/SelenideExampleTest.java
+++ src/test/java/selenideexample/SelenideExampleTest.java
@@ -1,5 +1,6 @@
 package selenideexample;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.Test;
@@ -14,6 +15,7 @@ public class SelenideExampleTest {
 
     @Test
     public void selenideTest() {
+        Configuration.remote = "http://localhost:4444/";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
         open("https://google.com");
         $("[name='q']")
